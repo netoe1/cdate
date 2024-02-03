b4fae4fa8365c518787852932d9201407bb117df
netoe1 <elynetobr@outlook.com>
2024-02-03 18:55:20 -0300

    remixing...

---
+++ src/clocktime.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "clocktime.h"
+#define UNDEFINED -1
---
+++ src/clocktime.h
@@ -0,0 +1,24 @@
+#ifndef CLOCKTIME_H
+#define CLOCKTIME_H
+
+#pragma region CDATE_STRUCT
+
+typedef struct
+{
+	int day;
+	int month;
+	int year;
+	int hour;
+	int min;
+	int sec;
+	int ms;
+} CDATE_Date;
+
+#pragma endregion CDATE_STRUCT
+
+#pragma region CDATE_FUNCTIONS
+
+
+#pragma endregion CDATE_FUNCTIONS
+
+#endif
